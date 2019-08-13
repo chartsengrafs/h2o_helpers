bdf955fb1ac87984b91f3ac02f1b24bc005b548a
chartsengrafs <austin_erik@yahoo.com>
2019-08-13 15:51:13 -0700

    display ROC

---
+++ h2o_py_roc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 15:49:46 2019
+
+@author: case
+"""
+
+perf = mymodel.model_performance()  #train=True is the default, so it's not needed
+perf.plot()
+
+https://github.com/h2oai/h2o-tutorials/issues/50
